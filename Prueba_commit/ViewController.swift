b675be57d623934559a76e656cb1f51f88ef6196
Rubén Francisco Romero Aguado <ruben_.fco@hotmail.com>
2019-03-13 11:21:51 -0600

    add date

--- Prueba_commit/ViewController.swift
+++ Prueba_commit/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Se carga la vista")
+        let today = NSDate()
+        print(today)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
